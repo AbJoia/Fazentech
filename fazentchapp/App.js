cbd5daa13325a17b75344b2ec60687ae62fdbf48
Abner Joia <abnerjoia1991@gmail.com>
2020-10-28 22:05:44 -0300

    pagina signup

--- fazentchapp/App.js
+++ fazentchapp/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Text} from 'react-native';
 import Home from './src/pages/Home/Home';
+import Signup from './src/pages/Signup/Signup';
 
 const App = () => {
   return (
     <>
-      <Home/>
+      <Signup/>
     </>
   );
 };
