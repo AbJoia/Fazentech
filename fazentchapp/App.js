f2db1a0a8749c930236ff99f71915cfb10bad05b
Abner Joia <abnerjoia1991@gmail.com>
2020-10-28 21:59:49 -0300

    criação do projeto e pagina home criada

---
+++ fazentchapp/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Home from './src/pages/Home/Home';
+
+const App = () => {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+};
+export default App;
