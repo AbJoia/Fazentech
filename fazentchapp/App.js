2ec1f774dae60f8c3059cbc4d6d447f7821f63ba
Abner Joia <abnerjoia1991@gmail.com>
2020-10-29 22:46:19 -0300

    cor statusbar

--- fazentchapp/App.js
+++ fazentchapp/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StatusBar} from 'react-native';
 
 import Home from './src/pages/Home';
 import Signup from './src/pages/Signup';
@@ -10,15 +11,21 @@ import OrderReview from './src/pages/OrderReview';
 
 const Tab = createBottomTabNavigator();
 
+
 const App = () => {
+
+ 
   return (
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Signup" component={Signup}/>
-        <Tab.Screen name="OrderReview" component={OrderReview}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar backgroundColor={'#222626'} />
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Home">
+          <Tab.Screen name="Home" component={Home}/>
+          <Tab.Screen name="Signup" component={Signup}/>
+          <Tab.Screen name="OrderReview" component={OrderReview}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 export default App;
