0df00cf9610dced683182aa2f9a408ee3191e2c2
Abner Joia <abnerjoia1991@gmail.com>
2020-10-31 19:09:30 -0300

    navigationbar component

--- fazentchapp/App.js
+++ fazentchapp/App.js
@@ -1,31 +1,9 @@
 import React from 'react';
+import NavigationBar from './src/Components/NavigationBar';
 
-import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {StatusBar} from 'react-native';
-
-import Home from './src/pages/Home';
-import Signup from './src/pages/Signup';
-import OrderReview from './src/pages/OrderReview';
-
-const Tab = createBottomTabNavigator();
-
-
-const App = () => {
-
- 
+const App = () => { 
   return (
-    <>
-      <StatusBar backgroundColor={'#222626'} />
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Home">
-          <Tab.Screen name="Home" component={Home}/>
-          <Tab.Screen name="Signup" component={Signup}/>
-          <Tab.Screen name="OrderReview" component={OrderReview}/>
-        </Tab.Navigator>
-      </NavigationContainer>
-    </>
+    <NavigationBar/>
   );
 };
 export default App;
---
+++ fazentchapp/src/Components/NavigationBar/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StatusBar} from 'react-native';
+
+import Home from '../../pages/Home';
+import Signup from '../../pages/Signup';
+import OrderReview from '../../pages/OrderReview';
+
+const Tab = createBottomTabNavigator();
+
+const NavigationBar = () =>{
+    return(
+      <>
+        <StatusBar backgroundColor={'#222626'} />
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen name="Home" component={Home}/>
+            <Tab.Screen name="Signup" component={Signup}/>
+            <Tab.Screen name="OrderReview" component={OrderReview}/>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </>
+    );
+}
+
+export default NavigationBar;
